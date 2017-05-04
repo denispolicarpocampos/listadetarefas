cd9228d37013a14ec5672ea30de8bf4633e949b9
Denis Policarpo Campos <campos_bra@hotmail.com>
2017-05-04 17:54:37 -0300

    Interface

--- app/models/tarefa.rb
+++ app/models/tarefa.rb
@@ -1,6 +1,7 @@
 class Tarefa < ApplicationRecord
 	belongs_to :user
-	validates :titulo, presence: true,
-                    length: { minimum: 5 }
+	validates :titulo, presence: true, length: { minimum: 5 }
+	validates :data, presence: true
+
     scope :complete, -> { where(complete: true) }
 end
