1e4c5b584f6885d5190f4c13187e9a3c2776692d
denispolicarpocampos <campos_bra@hotmail.com>
2017-05-09 00:03:01 -0300

    Ajustando autenticações de formulários

--- app/models/tarefa.rb
+++ app/models/tarefa.rb
@@ -3,14 +3,7 @@ class Tarefa < ApplicationRecord
 	validates_presence_of :titulo, message: 'não pode ficar em branco'
 	validates_presence_of :data, message: 'e hora devem ser preenchidas'
 	validates_length_of :titulo, minimum: 5, message: 'deve ter no mínimo 5 caracteres'
-	validate :past_date
     scope :complete, -> { where(complete: true) }
-
-    def past_date
-    	if self.data < Date.today
-    		errors.add( :data, 'não pode ser no passado')
-    	end
-    end
 
 
 end
